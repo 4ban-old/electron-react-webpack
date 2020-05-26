4560886d32f1321e491affcef1fd62ce093a1fe7
Dmitry Kryukov <remasik@gmail.com>
2020-05-25 19:17:58 -0700

    Add demonstration of IPC to the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { ipcRenderer } from "electron";
 
 const App = () => {
+  const [version, setVersion] = useState("unknown");
   useEffect(() => {
     // Enable listener
     ipcRenderer.on("test-renderer", (event, version) => {
-      console.log("App version", version);
+      setVersion(version);
     });
     // Send event to main process
     ipcRenderer.send("test-main");
   });
-  return <div>Hello, I'm an Electron application</div>;
+  return <div>Hello, I'm an Electron application version: {version}</div>;
 };
 
 export default App;
